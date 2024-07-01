1393cdb3e343b22baba9afc2c6f5911bdc2817f5
tyuukau <bachocnhi2002@gmail.com>
2024-07-01 14:05:16 +0700

    [code] ~ utils/__init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,6 @@
-from chatbot_utils import generate_response
-from object_detection_utils import process_image, annotate_image
-from word_correction_utils import levenshtein_distance, load_vocab
+from .chatbot_utils import generate_response
+from .object_detection_utils import process_image, annotate_image
+from .word_correction_utils import levenshtein_distance, load_vocab
 
 
 __all__ = [
